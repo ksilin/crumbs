571c19c7c16fc80a1ef15e92d612eaf41aa2a086
ksilin <konstantin.silin@gmail.com>
2013-12-12 16:27:59 +0100

    adedd examples for hash filtering with and without active support

--- spec/hash_filtering_spec.rb
+++ spec/hash_filtering_spec.rb
@@ -1,12 +1,27 @@
-require 'active_support'
 
-describe 'filtering a hash' do
+describe 'filtering a vanilla hash' do
 
   let(:h){{:key1 => 'foo', :to_removeY => 'baz', :key2 => 'bar', :to_removeX => 'baz'}}
+  let(:result){{:key1 => 'foo', :key2 => 'bar'}}
 
-  it 'should be filterable by keys' do
-    p h.reject{|k, v| k =~/remove/}
-    p h.collect{|k, v| p "#{k}: #{v}"}
+  it 'should be filterable by :reject' do
+    filtered = h.reject { |k, v| k =~/remove/ }
+    expect(filtered).to eq result
+  end
+end
+
+require 'active_support/core_ext/hash/slice'
+
+describe 'filtering with activesupport' do
+
+  let(:h){{:key1 => 'foo', :to_removeY => 'baz', :key2 => 'bar', :to_removeX => 'baz'}}
+  let(:result){{:key1 => 'foo', :key2 => 'bar'}}
+
+  it 'should be filterable by :slice' do
+
+    # with slice, you name the keys you want to keep
+    filtered = h.slice(:key1, :key2)
+    expect(filtered).to eq result
   end
 
 end
